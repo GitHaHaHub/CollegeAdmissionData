f127a8779af37f351493648f3abf80b0796bd75d
haha <swidhaha@outlook.com>
2024-10-14 17:23:36 +0900

    server running check @CAD-11

---
+++ backend_server/src/main/java/college_admission_data/backend_server/HomeController.java
@@ -0,0 +1,12 @@
+package college_admission_data.backend_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
